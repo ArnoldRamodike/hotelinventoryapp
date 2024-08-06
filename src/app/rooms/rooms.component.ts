035824990d04230338193f9f18be2a06f1d3d34d
ArnoldRamodike <riccoarnold23@gmail.com>
2024-08-06 22:50:06 +0200

    Use service to get data

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -3,6 +3,7 @@ import { Room, RoomList } from './rooms';
 import { CommonModule } from '@angular/common';
 import { RoomListComponent } from './room-list/room-list.component';
 import { HeaderComponent } from "../header/header.component";
+import { RoomsService } from './rooms.service';
 
 
 @Component({
@@ -12,59 +13,31 @@ import { HeaderComponent } from "../header/header.component";
   templateUrl: './rooms.component.html',
   styleUrl: './rooms.component.css'
 })
-export class RoomsComponent implements OnInit, AfterViewInit  {
+export class RoomsComponent implements OnInit, AfterViewInit {
   hotelName = "Mariot hotel";
   numberOfRoom = 10;
 
-  rooms : Room ={
+  rooms: Room = {
     availableRooms: 10,
     bookedRooms: 5,
     totalRooms: 20
   }
 
-  selectedRoom! : RoomList;
+  selectedRoom!: RoomList;
 
-  roomLists : RoomList[] = [ ]
+  roomLists: RoomList[] = []
 
- constructor () { }
 
- @ViewChild(HeaderComponent, {static: true}) headerComponent! : HeaderComponent;
+  @ViewChild(HeaderComponent, { static: true }) headerComponent!: HeaderComponent;
 
- @ViewChild(HeaderComponent) HeaderChildrenComponent!: QueryList<HeaderComponent>
+  @ViewChild(HeaderComponent) HeaderChildrenComponent!: QueryList<HeaderComponent>
 
-  ngOnInit() : void {
-    this.roomLists = [
-      {
-        roomNumber: 2,
-      roomType: "delux",
-      ameneties: "luncjh",
-      price: 200,
-      photos: "https://images.unsplash.com/photo-1722232581651-d87e4099561b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
-      checkinTime: new Date("23-apr-2024"),
-      checkoutTime: new Date( "11-nov-2023"),
-        rating: 4.5
-    },
-      {
-        roomNumber: 5,
-      roomType: "plain",
-      ameneties: "nne",
-      price: 200,
-      photos: "https://plus.unsplash.com/premium_photo-1722170080353-5b68cd7b16ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
-      checkinTime: new Date("27-apr-2024"),
-      checkoutTime: new Date( "11-may-2023"),
-        rating: 5.0
-    },
-      {
-        roomNumber: 10,
-      roomType: "Good",
-      ameneties: "wifi",
-      price: 120,
-      photos: "https://plus.unsplash.com/premium_photo-1722170080353-5b68cd7b16ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
-      checkinTime: new Date("27-jan-2024"),
-      checkoutTime: new Date( "11-dec-2025"),
-        rating: 3.5
-    },
-    ]
+  // roomService  = new RoomService();
+
+  constructor(private roomsService: RoomsService) { }
+
+  ngOnInit(): void {
+    this.roomLists = this.roomsService.getRooms();
   }
 
   ngAfterViewInit(): void {
@@ -73,14 +46,14 @@ export class RoomsComponent implements OnInit, AfterViewInit  {
     this.HeaderChildrenComponent.last.title = "LAnce stroll!"
   }
 
-  hidrooms =false;
+  hidrooms = false;
   title = "Room Lists"
-  toggle(){
+  toggle() {
     this.hidrooms = !this.hidrooms;
     this.title = "Room Changed"
   }
 
-  selectRoom(room: RoomList){
+  selectRoom(room: RoomList) {
     this.selectedRoom = room;
   }
 
@@ -92,8 +65,8 @@ export class RoomsComponent implements OnInit, AfterViewInit  {
       price: 200,
       photos: "https://images.unsplash.com/photo-1722232581651-d87e4099561b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
       checkinTime: new Date("26-aug-2024"),
-      checkoutTime: new Date( "21-nov-2023"),
-        rating: 2.5
+      checkoutTime: new Date("21-nov-2023"),
+      rating: 2.5
     };
 
     // this.roomLists.push(room);
---
+++ src/app/rooms/rooms.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@angular/core';
+import { RoomList } from './rooms';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoomsService {
+
+  roomLists: RoomList[] = [
+    {
+      roomNumber: 2,
+      roomType: "delux",
+      ameneties: "luncjh",
+      price: 200,
+      photos: "https://images.unsplash.com/photo-1722232581651-d87e4099561b?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8",
+      checkinTime: new Date("23-apr-2024"),
+      checkoutTime: new Date("11-nov-2023"),
+      rating: 4.5
+    },
+    {
+      roomNumber: 5,
+      roomType: "plain",
+      ameneties: "nne",
+      price: 200,
+      photos: "https://plus.unsplash.com/premium_photo-1722170080353-5b68cd7b16ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+      checkinTime: new Date("27-apr-2024"),
+      checkoutTime: new Date("11-may-2023"),
+      rating: 5.0
+    },
+    {
+      roomNumber: 10,
+      roomType: "Good",
+      ameneties: "wifi",
+      price: 120,
+      photos: "https://plus.unsplash.com/premium_photo-1722170080353-5b68cd7b16ff?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwyfHx8ZW58MHx8fHx8",
+      checkinTime: new Date("27-jan-2024"),
+      checkoutTime: new Date("11-dec-2025"),
+      rating: 3.5
+    },
+  ]
+  constructor() { }
+
+  getRooms() {
+    return this.roomLists;
+  }
+}
